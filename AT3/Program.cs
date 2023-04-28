8cc1f728b5edf54417d9fc9a7e584cfbee310582
ConvenientQuota <115048910+ConvenientQuota@users.noreply.github.com>
2023-04-28 11:16:19 +1000

    Update Program.cs
    added functions

--- AT3/Program.cs
+++ AT3/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,10 +10,35 @@ namespace AT3
     {
         static void Main(string[] args)
         {
+            ResearcherController control = new ResearcherController();
             List<Researcher> researchers = Data.GenerateResearcher();
             Console.WriteLine("All current researchers\n");
             DisplayResearcher(researchers);
+
+            //add researcher
+            Researcher researcher1 = new Researcher();  
+            control.addResearcher(researcher1);
+
+            //get all researchers
+            List<Researcher> researcher = control.addResearcher();
+            foreach (Researcher researcher in researchers)
+            {
+                control.addResearcher(researcher);
+            }
+
+            //remove researcher
+            control.removeResearcher(researcher1);
+
+            //Filter By Name
+            Researcher researcher2 = control.filterByName("Alex");
+            if (researcher2 != null)
+            {
+                control.DisplayResearcherDetails(researcher2);
+            }
+
+            PublicationController
         }
+
 
         static void DisplayResearcher(List<Researcher> a)
         {
