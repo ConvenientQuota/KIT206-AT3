5108616782f9f1d31eef051338a27bedf61bc580
HakkiMcSnakki213 <harkirat.grewalhs.hg@gmail.com>
2023-05-05 11:33:57 +1000

    changed type of campus

--- AT3/Entity/Researcher.cs
+++ AT3/Entity/Researcher.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace AT3
 {
     public enum EmployeeLevel { A, B, C, D, E }
+    public enum Campus { Hobart, Launceston, CradleCoast}
 
     public class Researcher
     {
@@ -16,7 +17,7 @@ namespace AT3
         public string Title { get; set; }
         public string School { get; set; }
         public string Unit { get; set; }
-        public string Campus { get; set; }
+        public Campus campus { get; set; }
         public string Email { get; set; }
         public EmployeeLevel Level { get; set; }
         public string commenceWithInstitute { get; set; }
