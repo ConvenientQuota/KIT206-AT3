13b06eb0c7ea1f9bd21f94fa8f7dab97f39e7401
HenryDeskTop <crofthans@yahoo.com.au>
2023-05-22 18:52:00 +1000

    Fixed findpublication funciton

--- AT3/Program.cs
+++ AT3/Program.cs
@@ -23,12 +23,13 @@ namespace AT3
             //Loading all researchers
             DbAdaptor.LoadAll();
             //Loading all publications
-            //DbAdaptor.LoadPublication();
+            DbAdaptor.LoadPublication();
             //Selecting a researcher based on their 'id'
-            //int id = 123460;
-            //DbAdaptor.ResearcherId(id);
+            int id = 123460;
+           DbAdaptor.ResearcherId(id);
+            string doi = "prefix/sample9";
+           DbAdaptor.FindPublication(doi);
 
-            //DbAdaptor.FindPublication(10.1007 / 11839088_42);
             /*
             //Add publications
             Publication removeExample = publicationControllers.LinqAddPublication(new Publication("Title", new List<String> { "Author1" }, 2020));
--- WpfApp1/WpfApp1/Controllers/ResearcherControllers.cs
+++ WpfApp1/WpfApp1/Controllers/ResearcherControllers.cs
@@ -10,6 +10,8 @@ namespace AT3.Controllers
     {
         public List<Researcher> researchers { get; }
 
+       
+
         public ResearcherController()
         {
             researchers = new List<Researcher>();
